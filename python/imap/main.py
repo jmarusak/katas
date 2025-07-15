7d91ebfc92e20b6e02c9ba0c15671bd49b931ca5
jmarusak <jmarusak@users.noreply.github.com>
2025-07-14 22:21:03 -0400

    refactor: Use environment variables for IMAP credentials and inbox

--- python/imap/main.py
+++ python/imap/main.py
@@ -1,12 +1,16 @@
+import os
 import imaplib
 import email
 
+EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+
 # Connect to Yahoo Mail
 mail = imaplib.IMAP4_SSL("imap.mail.yahoo.com")
-mail.login("<email>", "<app password>")
+mail.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 
 # Select inbox
-mail.select("agents")
+mail.select("inbox")
 
 # Search all emails
 status, messages = mail.search(None, "ALL")
@@ -16,8 +20,11 @@ for num in messages[0].split():
     raw_email = data[0][1]
     msg = email.message_from_bytes(raw_email)
 
-    # Get email content
+    # Get email header 
     subject = msg["subject"]
     from_ = msg["from"]
+
+    # Get email body
+    body = ...
 
     print(from_)
